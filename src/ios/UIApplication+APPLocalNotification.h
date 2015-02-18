e6d254b5d62736ce42432a07f96da3521f7d865d
Avi Vaisenberger <avi@fabulagame.com>
2015-02-18 14:22:35 +0200

    Correct iOS8 identification

--- src/ios/UIApplication+APPLocalNotification.h
+++ src/ios/UIApplication+APPLocalNotification.h
@@ -23,6 +23,8 @@
 
 #import "UILocalNotification+APPLocalNotification.h"
 
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
 @interface UIApplication (APPLocalNotification)
 
 @property (readonly, getter=localNotifications) NSArray* localNotifications;
